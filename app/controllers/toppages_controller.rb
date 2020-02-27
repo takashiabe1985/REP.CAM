3d36b837e70ce88743c542e0a87ea15679f8498c
dani.camp <bboydanee@gmail.com>
2020-02-27 07:24:59 +0000

    user posts

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,7 +1,7 @@
 class ToppagesController < ApplicationController
   def index
     if logged_in?
-      @posts = current_user.microposts.order(id: :desc).page(params[:page])
+      @posts = current_user.posts.order(id: :desc).page(params[:page])
     end
   end
 end
