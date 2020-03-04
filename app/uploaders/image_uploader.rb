d685cef6de3d0c30b474e907b7f596a332b284b6
dani.camp <bboydanee@gmail.com>
2020-03-04 03:43:10 +0000

    rmagick

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,7 +1,12 @@
 class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
-  # include CarrierWave::RMagick
+   include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
+  
+  process :resize_to_limit => [640, 480]
+  process :convert => 'jpg'
+  
+  
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -29,19 +34,38 @@ class ImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process resize_to_fit: [50, 50]
-  # end
+   version :thumb do
+     process :resize_to_limit => [300, 300]
+  end
+  
+   version :thumb do
+     process :resize_to_limit => [100, 100]
+  end
+  
+   version :thumb do
+     process :resize_to_limit => [30, 30]
+  end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_whitelist
-  #   %w(jpg jpeg gif png)
-  # end
+   def extension_whitelist
+    %w(jpg jpeg gif png)
+   end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
-  # def filename
-  #   "something.jpg" if original_filename
-  # end
+   def filename
+     super.chomp(File.extname(super) ) + '.jpg' if original_filename.present?
+   end
+   
+   def filename
+     "#{secure_token}.#{file.extension}" if original_filename.present?
+   end
+   
+   protected
+   def secure_token
+     var = :"@#{mounted_as}_secure_token"
+     model.instance_variable_get(var) or model.instance_variable_set(var, SecureRandom.uuid)
+   end 
+   
 end
